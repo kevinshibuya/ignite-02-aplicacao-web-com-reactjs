ec3224dca753951ae7f035b4d2d11f9a31d13bb3
Kevin Shibuya <kevinshibuya@hotmail.com>
2022-01-24 20:01:46 -0300

    modal structure

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +11,28 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      className="react-modal-content"
+      overlayClassName="react-modal-overlay"
     >
-      <h2>Register transaction</h2>
+      <Container>
+        <h2>Register transaction</h2>
+
+        <input
+         type="text"
+         placeholder="Title"
+        />
+        <input
+         type="number"
+         placeholder="Value"
+        />
+        <input
+         type="text"
+         placeholder="Category"
+        />
+        <button type="submit">
+          Register
+        </button>
+      </Container>
     </Modal>
   )
 }
