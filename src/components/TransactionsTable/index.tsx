65643992686da6d0f8dc2821b70415fa65aed341
Kevin Shibuya <kevinshibuya@hotmail.com>
2022-01-24 13:14:33 -0300

    creating summary component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Value</th>
+            <th>Category</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Website development</td>
+            <td className="deposit">R$12.000,00</td>
+            <td>Development</td>
+            <td>21/01/2022</td>
+          </tr>
+          <tr>
+            <td>Rent</td>
+            <td className="withdraw">- R$3.000,00</td>
+            <td>Home</td>
+            <td>01/01/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
