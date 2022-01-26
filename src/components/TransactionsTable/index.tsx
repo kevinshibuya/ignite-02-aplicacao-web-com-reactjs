afa4caf22ac92760e29b800ca19a3854c31de50a
Kevin Shibuya <kevinshibuya@hotmail.com>
2022-01-26 12:09:12 -0300

    getting data from the api and setting default data to miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
     api.get('/transactions')
-      .then(response => console.log(response.data));
+      .then(response => setTransactions(response.data.transactions));
   }, []);
 
   return (
@@ -22,18 +33,16 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Website development</td>
-            <td className="deposit">R$12.000,00</td>
-            <td>Development</td>
-            <td>21/01/2022</td>
-          </tr>
-          <tr>
-            <td>Rent</td>
-            <td className="withdraw">- R$3.000,00</td>
-            <td>Home</td>
-            <td>01/01/2022</td>
-          </tr>
+          {transactions.map(transaction => {
+            return (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>{transaction.amount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Website freelance',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 2000,
+          createdAt: new Date('2022-01-14 08:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Rent',
+          type: 'withdraw',
+          category: 'House',
+          amount: 350,
+          createdAt: new Date('2022-01-01 08:00:00'),
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
