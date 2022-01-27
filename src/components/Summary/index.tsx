6378937a7dec16bdf8cc5e944501aa0b8205158e
Kevin Shibuya <kevinshibuya@hotmail.com>
2022-01-27 20:13:48 -0300

    calculating summary values

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,16 +1,30 @@
 import { useContext } from 'react';
 import { TransactionsContext } from '../../TransactionsContext';
 
-import incomeImg from'../../assets/income.svg';
-import outcomeImg from'../../assets/outcome.svg';
-import totalImg from'../../assets/total.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
 
 import { Container } from "./styles";
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
-  console.log(transactions)
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposit += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraw += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+  }, {
+    withdraw: 0,
+    deposit: 0,
+    total: 0
+  });
 
   return (
     <Container>
@@ -19,21 +33,37 @@ export function Summary() {
           <p>Income</p>
           <img src={incomeImg} alt="" />
         </header>
-        <strong>R$1000</strong>
+        <strong>
+          {new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'AUD'
+          }).format(summary.deposit)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Outcome</p>
           <img src={outcomeImg} alt="" />
         </header>
-        <strong>-R$500</strong>
+        <strong>
+          -
+          {new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'AUD'
+          }).format(summary.withdraw)}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="" />
         </header>
-        <strong>R$500</strong>
+        <strong>
+          {new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'AUD'
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   )
